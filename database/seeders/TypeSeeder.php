dbe9b2933af4e3d47317e56d15e46ba68c828f87
Therakar <matteo.pau200@gmail.com>
2023-01-30 21:21:02 +0100

    popolamento seeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Type;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -15,6 +16,8 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
+        Type::truncate(); //cancella tutti i dati della tabella Types
+
         $types = [
             'Business implementation',
             'Foundational (business improvement)',
@@ -29,5 +32,12 @@ class TypeSeeder extends Seeder
             'Service development',
             'Transformation/reengineering'
         ];
+
+        foreach($types as $type) {
+            $new_type = new Type();
+            $new_type->name = $type;
+            $new_type->slug = Str::slug($new_type->name);
+            $new_type->save();
+        }
     }
 }
