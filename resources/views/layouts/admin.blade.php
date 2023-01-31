a18b04a257a4630b623660f6363dc7c65759e320
Therakar <matteo.pau200@gmail.com>
2023-01-31 01:17:02 +0100

    creo icona aside

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -100,6 +100,13 @@
                     </a>
                 </li>
 
+                <li class="nav-item">
+                  <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.types.index' ? 'bg-secondary' : '' }}" href="{{route('admin.types.index')}}">
+                      <i class="fa-solid fa-tag fa-lg fa-fw"></i>
+                      Types
+                  </a>
+              </li>
+
               </ul>
             </div>
           </nav>
